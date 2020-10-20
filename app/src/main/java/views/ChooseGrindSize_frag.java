102379b2dc632d6e7d84fd15a79f5460b2621970
gustavkirkholt <kirkholtgustav@gmail.com>
2020-10-20 12:47:00 +0200

    Ændret fragment import

--- app/src/main/java/views/ChooseGrindSize_frag.java
+++ app/src/main/java/views/ChooseGrindSize_frag.java
@@ -1,8 +1,7 @@
 package views;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
+import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;

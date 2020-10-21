7661980721e218a4a3f8c4464eea6fc0dadd76b3
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-10-21 09:46:00 +0200

    Update fragment to use / support double and use PreferenceHelper

--- app/src/main/java/views/ChooseAmountOfCoffee_frag.java
+++ app/src/main/java/views/ChooseAmountOfCoffee_frag.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import dk.dtu.gruppeb3.broeg.app.R;
-import models.Application;
+import helpers.PreferenceHelper;
 
 /**
  * This fragment gives the user the opportunity to choose the amount of coffee they want to brew.
@@ -24,8 +23,7 @@ public class ChooseAmountOfCoffee_frag extends Fragment implements View.OnClickL
 
     Button knap1, knap2, knap3;
     private View rod;
-    int amountOfCoffee;
-    SharedPreferences prefs;
+    double amountOfCoffee;
 
     @Override
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState){
@@ -39,8 +37,9 @@ public class ChooseAmountOfCoffee_frag extends Fragment implements View.OnClickL
         knap2.setOnClickListener(this);
         knap3.setOnClickListener(this);
 
-        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+//        this.amountOfCoffee = Double.parseDouble(preferences.getString("amountOfCoffee", "0"));
+        this.amountOfCoffee = PreferenceHelper.getDouble(preferences, "amountOfCoffee", "0");
 
         return rod;
 
@@ -63,7 +62,15 @@ public class ChooseAmountOfCoffee_frag extends Fragment implements View.OnClickL
             tv.setText("Mængde kaffe i gram (" +amountOfCoffee + ")");
 
         } else if (ButtonClick == knap3){
-            prefs.edit().putInt("Mængde kaffe", amountOfCoffee).apply();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+            // This will get a value from the preferences.
+            // int value = preferences.getInt("Key", amountOfCoffee);
+
+            // This will set a value in the preferences
+            // preferences.edit().putString("amountOfCoffee", Double.toString(this.amountOfCoffee)).apply();
+            PreferenceHelper.putDouble(preferences, "amountOfCoffee", this.amountOfCoffee);
+
             getActivity().onBackPressed();
         }
 

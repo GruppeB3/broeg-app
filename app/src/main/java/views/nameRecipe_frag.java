1e5362571da381c4adb62d9a83c997d0e7f1dcb5
gustavkirkholt <kirkholtgustav@gmail.com>
2020-10-23 09:48:22 +0200

    tilføjet gem knap og metode

--- app/src/main/java/views/nameRecipe_frag.java
+++ app/src/main/java/views/nameRecipe_frag.java
@@ -15,28 +15,37 @@ import android.widget.TextView;
 import dk.dtu.gruppeb3.broeg.app.R;
 import helpers.PreferenceHelper;
 
-public class nameRecipe_frag extends Fragment {
+public class nameRecipe_frag extends Fragment implements View.OnClickListener {
 
     private View rod;
     TextView tv;
     EditText et;
-    String opskriftNavn;
+    TextView opskriftNavn;
+    Button knap1;
 
 
     @Override
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
         this.rod = i.inflate(R.layout.activity_choose_amount_of_coffee, container, false);
 
+        knap1 = rod.findViewById(R.id.gemOpskrift);
+
+        knap1.setOnClickListener(this);
+
         et  = (EditText)rod.findViewById(R.id.nameRecipe);
         et.getText().toString();
 
-        tv = (TextView)rod.findViewById(R.id.nameRecipe);
-        tv.setText(et.getText().toString());
-
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-        preferences.edit().putString("GrindSize", this.opskriftNavn).apply();
-        getActivity().onBackPressed();
+        opskriftNavn = (TextView)rod.findViewById(R.id.nameRecipe);
+        opskriftNavn.setText(et.getText().toString());
 
         return rod;
+    }
+
+    @Override
+    public void onClick(View ButtonClick) {
+        if (ButtonClick == knap1){
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+            preferences.edit().putString("OpskriftNavn", String.valueOf(this.opskriftNavn)).apply();
+            getActivity().onBackPressed();
     }
 }

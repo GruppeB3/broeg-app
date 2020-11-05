2f0405bb8f7363f63e6487ad4c6115bba4e98ad2
gustavkirkholt <kirkholtgustav@gmail.com>
2020-11-05 18:54:22 +0100

    Tilføjet mulig metode til onLongClick

--- app/src/main/java/views/ChooseAmountOfCoffee_frag.java
+++ app/src/main/java/views/ChooseAmountOfCoffee_frag.java
@@ -1,15 +1,19 @@
 package views;
 
 import android.app.Fragment;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import dk.dtu.gruppeb3.broeg.app.R;
 import helpers.PreferenceHelper;
@@ -18,15 +22,18 @@ import helpers.PreferenceHelper;
  * This fragment gives the user the opportunity to choose the amount of coffee they want to brew.
  */
 
-public class ChooseAmountOfCoffee_frag extends Fragment implements View.OnClickListener {
+public class ChooseAmountOfCoffee_frag extends Fragment implements View.OnClickListener, View.OnLongClickListener {
 
     Button knap1, knap2, knap3;
     private View rod;
     double amountOfCoffee;
+    private boolean mAutoIncrement = false;
+    private boolean mAutoDecrement = false;
+    public int mValue;
 
     @Override
-    public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState){
-        this.rod= i.inflate(R.layout.activity_choose_amount_of_coffee, container, false);
+    public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
+        this.rod = i.inflate(R.layout.activity_choose_amount_of_coffee, container, false);
 
         knap1 = rod.findViewById(R.id.ArrowUp_CoffeAmount);
         knap2 = rod.findViewById(R.id.ArrowDown_CoffeAmount);
@@ -41,22 +48,22 @@ public class ChooseAmountOfCoffee_frag extends Fragment implements View.OnClickL
 
     }
 
+
     @Override
     public void onClick(View ButtonClick) {
-
 
 
         if (ButtonClick == knap1) {
             amountOfCoffee++;
             TextView tv = rod.findViewById(R.id.CoffeeAmount);
-            tv.setText("Mængde kaffe i gram (" +amountOfCoffee + ")");
+            tv.setText("Mængde kaffe i gram (" + amountOfCoffee + ")");
 
-        } else if (ButtonClick == knap2){
+        } else if (ButtonClick == knap2) {
             amountOfCoffee--;
             TextView tv = rod.findViewById(R.id.CoffeeAmount);
-            tv.setText("Mængde kaffe i gram (" +amountOfCoffee + ")");
+            tv.setText("Mængde kaffe i gram (" + amountOfCoffee + ")");
 
-        } else if (ButtonClick == knap3){
+        } else if (ButtonClick == knap3) {
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
             PreferenceHelper.putDouble(preferences, "amountOfCoffee", this.amountOfCoffee);
 
@@ -64,4 +71,25 @@ public class ChooseAmountOfCoffee_frag extends Fragment implements View.OnClickL
         }
 
     }
+
+    @Override
+    public boolean onLongClick(View LongButtonClick) {
+
+        //   final Timer timer = new Timer();
+        // timer.schedule(new TimerTask() {
+        //   @Override
+        // public void run() {
+        //   if (knap1 == LongButtonClick) {
+        //     amountOfCoffee++;
+        //  } else timer.cancel();
+        //  }
+
+        return false;
+    }
 }
+
+
+        //TODO få metoden til at gentage sig selv hvert 0,5 sekund knappet er holdt nede.
+
+
+

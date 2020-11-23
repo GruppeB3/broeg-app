7d8a269e3c5d2f8e393ed4441a961ff9b1ddb537
gustavkirkholt <kirkholtgustav@gmail.com>
2020-11-23 10:35:01 +0100

    tilføjet MyProfile knap til forside

--- app/src/main/java/views/activities/MainActivity.java
+++ app/src/main/java/views/activities/MainActivity.java
@@ -11,16 +11,18 @@ import dk.dtu.gruppeb3.broeg.app.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button newBrewButton, cleaningButton;
+    Button newBrewButton, cleaningButton, profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        profileButton = findViewById(R.id.profile_button);
         newBrewButton = findViewById(R.id.broeg_frontpagebutton);
         cleaningButton = findViewById(R.id.cleaning_button);
 
+        profileButton.setOnClickListener(this);
         newBrewButton.setOnClickListener(this);
         cleaningButton.setOnClickListener(this);
     }
@@ -37,6 +39,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             startActivity(new Intent(this, CleaningActivity.class));
 
+        } else if (v == profileButton){
+
+            Intent i = new Intent(this, MyProfile.class);
+            startActivity(i);
         }
 
     }
---
+++ app/src/main/java/views/activities/MyProfile.java
@@ -0,0 +1,16 @@
+package views.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dk.dtu.gruppeb3.broeg.app.R;
+
+public class MyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}

ace7d59660842bde3ff40dfbd8d19c1ea10b9f38
gustavkirkholt <kirkholtgustav@gmail.com>
2021-01-07 18:26:09 +0100

    ExampleDialog klasse

---
+++ app/src/main/java/models/ExampleDialog.java
@@ -0,0 +1,16 @@
+package models;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+    }
+}

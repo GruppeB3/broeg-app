cea4a451d881c46cfd370e2afff7dbcfb3d5a52b
gustavkirkholt <kirkholtgustav@gmail.com>
2021-01-07 18:34:29 +0100

    .

--- app/src/main/java/models/ExampleDialog.java
+++ app/src/main/java/models/ExampleDialog.java
@@ -1,16 +1,22 @@
 package models;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import dk.dtu.gruppeb3.broeg.app.R;
+
 public class ExampleDialog extends AppCompatDialogFragment {
-    @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = inflater.inflate(R.layout.brew_dialog, null);
 
+        builder.setView(view)
     }
 }
